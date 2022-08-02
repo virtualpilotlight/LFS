b815932604f92d50a400bd964c01b9eb34a47925
Omega X Rae <omega.x.rae@gmail.com>
2022-08-01 21:12:20 -0700

    altered the script for easier readability

--- version-check1.sh
+++ version-check1.sh
@@ -8,9 +8,24 @@ echo $MYSH | grep -q bash || echo "ERROR: /bin/sh does not point to bash"
 unset MYSH
 
 apt-get install binutils
-echo -n "Binutils: "; ld --version | head -n1 | cut -d" " -f3-
 apt-get install bison
-bison --version | head -n1
+apt-get install coreutilis
+apt-get install diffutils
+apt-get install find
+apt-get install gawk
+apt-get install gcc
+apt-get install g++
+apt-get install grep
+apt-get install gzip
+apt-get install m4
+apt-get install make
+apt-get install patch
+apt-get install perl
+apt-get install python3
+apt-get install sed
+apt-get install tar
+apt-get install makeinfo
+apt-get install xz
 
 if [ -h /usr/bin/yacc ]; then
   echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`";
@@ -20,15 +35,6 @@ else
   apt-get install yacc
 fi
 
-apt-get install coreutilis
-echo -n "Coreutils: "; chown --version | head -n1 | cut -d")" -f2
-apt-get install diffutils
-diff --version | head -n1
-apt-get install find
-find --version | head -n1
-apt-get install gawk
-gawk --version | head -n1
-
 if [ -h /usr/bin/awk ]; then
   echo "/usr/bin/awk -> `readlink -f /usr/bin/awk`";
 elif [ -x /usr/bin/awk ]; then
@@ -37,32 +43,25 @@ else
   apt-get install awk
 fi
 
-apt-get install gcc
+echo -n "Binutils: "; ld --version | head -n1 | cut -d" " -f3-
+bison --version | head -n1
+echo -n "Coreutils: "; chown --version | head -n1 | cut -d")" -f2
+diff --version | head -n1
+find --version | head -n1
+gawk --version | head -n1
 gcc --version | head -n1
-apt-get install g++
 g++ --version | head -n1
-apt-get install grep
 grep --version | head -n1
-apt-get install gzip
 gzip --version | head -n1
 cat /proc/version
-apt-get install m4
 m4 --version | head -n1
-apt-get install make
 make --version | head -n1
-apt-get install patch
 patch --version | head -n1
-apt-get install perl
 echo Perl `perl -V:version`
-apt-get install python3
 python3 --version
-apt-get install sed
 sed --version | head -n1
-apt-get install tar
 tar --version | head -n1
-apt-get install makeinfo
 makeinfo --version | head -n1  # texinfo version
-apt-get install xz
 xz --version | head -n1
 
 echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
